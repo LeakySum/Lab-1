d849d1abe10492dc395ae8e94bc0b4360dec13c7
LeakySum <leakysum60@gmail.com>
2016-10-07 09:49:58 +0300

    Update Lab1_1.cpp

--- Lab1_1.cpp
+++ Lab1_1.cpp
@@ -2,16 +2,16 @@
 #include <math.h>
 
 using namespace std;
-// Эта программа решает систему уравнений y=sqrt(x-2)/(5+x^2) при x>=2 y=(x-2)/abs(5-x^2) если x<2
+// Эта программа решает систему уравнений y=(5+x^2)/sqrt(x-2) при x>=2 y=abs(5-x^2)/2*(x-2) если x<2
 int main() {
     double x,y;
     cout << "x=";
     cin >> x;
 
     if (x>=2)
-        y=sqrt(x-2)/(5+pow(x,2));
+        y=(5+pow(x,2))/sqrt(x-2);
     else
-        y=(x-2)/fabs(5-pow(x,2));
+        y=fabs(10-pow(x,2))/2*(x-2);
 
     cout << "y=" << y;
     return 0;
